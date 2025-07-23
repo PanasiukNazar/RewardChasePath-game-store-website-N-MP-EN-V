328d793e36b4d35e8d33e08f492f56acb7525866
Nazar Panasiuk <nazar.panasiuk.ua@gmail.com>
2025-07-23 16:14:29 +0300

    WIP

---
+++ products.js
@@ -0,0 +1,38 @@
+initStore([
+    {
+        id: 0,
+        title: 'Чорна ікра від виробника',
+        description:
+            'Чорна ікра преміум-класу, натуральна ікра без консервантів, у скляних банках.',
+        price: 799, // Adjusted price
+        discountPrice: 1399, // Adjusted price
+        image: 'img/service-1.jpg',
+    },
+    {
+        id: 2,
+        title: 'Свіжа червона ікра',
+        description:
+            'Свіжа червона ікра, без консервантів, від виробника, доставка по Україні.',
+        price: 499, // Adjusted price
+        discountPrice: 999, // Adjusted price
+        image: 'img/service-2.jpg',
+    },
+    {
+        id: 3,
+        title: 'Ікра преміум-класу',
+        description:
+            'Ікра преміум-класу для гурманів, натуральна ікра без консервантів, доставка по Україні.',
+        price: 599, // Adjusted price
+        discountPrice: 1249, // Adjusted price
+        image: 'img/service-3.jpg',
+    },
+    {
+        id: 4,
+        title: 'Подарункові набори з ікрою',
+        description:
+            'Подарункові набори з ікрою чорної та червоної ікри, ідеальний подарунок для гурманів.',
+        price: 1199, // Adjusted price
+        discountPrice: 2399, // Adjusted price
+        image: 'img/service-4.jpg',
+    },
+]);
